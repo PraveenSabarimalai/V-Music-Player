37c40e957f743c43f6dd55ce879f5671d21a4937
Praveen Sabarimalai <praveen@example.com>
2025-02-12 22:36:53 +0530

    files

---
+++ music-list.js
@@ -0,0 +1,64 @@
+let allMusic = [
+  {
+    name: "Whitsle Podu", 
+    artist: "Vijay || Yuvan Sankar Raja || Madhan Karky ",
+    img: "Song_1", 
+    src: "Song_1"  
+  },
+  {
+    name: " Nan Ready Than ",
+    artist: "Vijay || Aniruth || Vishnu Edvan",
+    img: "Song_2",
+    src: "Song_2"
+  },
+  {
+    name: "Kutty Story",
+    artist: "Vijay || Aniruth || Arunraja Kamaraj",
+    img: "Song_3",
+    src: "Song_3"
+  },
+  {
+    name: "Kandangi Kandangi",
+    artist: "Vijay Sheraya Ghoshal || D.Imman || Vairamuthu",
+    img: "Song_4",
+    src: "Song_4"
+  },
+  {
+    name: "Verithanam",
+    artist: "Vijay || A R Rahman || Vivek",
+    img: "Song_5",
+    src: "Song_5"
+  },
+  {
+    name: "Vankana Vanakamnga anna",
+    artist: "Vijay Santhanam || G V Prakash Kumar || Na.Muthukumar",
+    img: "Song_6",
+    src: "SOng_6"
+  },
+
+  {
+    name: "Vaadi Vaadi",
+    artist: "Vijay Vadivelu || Devi Sri Prasath",
+    img: "Song_7",
+    src: "Song_7"
+  },
+  {
+    name: "Enoda Lila",
+    artist: "Vijay || Ramana Gogula",
+    img: "Song_8",
+    src: "Song_8"
+  },
+  {
+    name: "Vikalalu Vikalalu",
+    artist: "Vijay || Deva || Kalai Kumar",
+    img: "Song_9",
+    src: "Song_9"
+  },
+  {
+    name: "papa papa",
+    artist: "Vijay || Santhosh Narayanan || Vairamuthu",
+    img: "Song_10",
+    src: "Song_10"
+  },
+
+];
